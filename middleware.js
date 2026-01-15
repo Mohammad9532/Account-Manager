6d648399d23b0eb7131d825c38b269195fffc704
Mohammad9532 <mohammadahmadsheikh580@gmail.com>
2026-01-15 13:24:50 +0400

    feat: Update middleware protected routes to include dashboard, ledgers, and income, and remove the root path.

--- middleware.js
+++ middleware.js
@@ -3,9 +3,11 @@ export default middleware;
 
 export const config = {
     matcher: [
-        "/",
+        "/dashboard",
         "/daily",
         "/reports",
+        "/ledgers",
+        "/income",
         // Add other protected routes here
     ]
 };
