94b1ad04ca7f3d876319576f37202a0ab79204d9
Mohammad9532 <mohammadahmadsheikh580@gmail.com>
2026-01-17 11:14:05 +0400

    feat: Add API routes for deleting and updating accounts by ID.

--- app/api/accounts/[id]/route.js
+++ app/api/accounts/[id]/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
-import dbConnect from '../../../../lib/db';
-import { Account } from '../../../../lib/models/Account';
+import dbConnect from "@/lib/db";
+import { Account } from "@/lib/models/Account";
 import { getServerSession } from 'next-auth';
 import { authOptions } from "@/lib/auth";
 
